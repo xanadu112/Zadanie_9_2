97f8feda69d878a50956f8df7767bd4f5099dbc6
Michał Zatorski <michal_zatorski@o2.pl>
2024-04-17 00:53:12 +0200

    Final 2

--- api_service_nbp.py
+++ api_service_nbp.py
@@ -1,15 +1,20 @@
 import requests
 import csv
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
+def get_write_rates():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
 
-with open('kursy.csv', 'w') as csvfile:
-    fieldnames = ['currency', 'code', 'bid', 'ask']
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-    csvwriter.writeheader()
+    with open('kursy.csv', 'w') as csvfile:
+        fieldnames = ['currency', 'code', 'bid', 'ask']
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        csvwriter.writeheader()
 
-    for n in data[0]["rates"]:
-        csvwriter.writerow(n)
+        for n in data[0]["rates"]:
+            csvwriter.writerow(n)
+
+if __name__ == '__main__':
+    get_write_rates()
+
 
 
--- current_calc.py
+++ current_calc.py
@@ -11,15 +11,22 @@ app = Flask(__name__)
 def curr_calc():
     curr_data = data[0]['rates']
     if request.method == 'GET':
-        return render_template("calc_form.html", curr_data=curr_data, transaction_cost=transaction_cost)  
+        return render_template("calc_form.html", curr_data=curr_data) 
+    
     elif request.method == 'POST':
-        select_curr = request.form("currencies")
+        select_curr = request.form.get("currencies")
+        exchange_amount = float(request.form.get("element"))
         
+        select_bid = None
         for i in data[0]["rates"]:
-            if i["currency"] == select_curr:
+            if i["code"] == select_curr:
                 select_bid = i["bid"]
+                break
         
-        exchange_amount = request.form("element")
-        transaction_cost = select_bid * exchange_amount
-        return redirect("/")
+        if select_bid is not None:
+            transaction_cost = select_bid * exchange_amount
+        else:
+            transaction_cost = 0
+        
+        return render_template("calc_form.html", curr_data=curr_data, transaction_cost=transaction_cost) 
     
