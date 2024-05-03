a4cf36b24d49bcb0ecfbdd68d8d6656f8e296420
Michał Zatorski <michal_zatorski@o2.pl>
2024-05-03 22:42:34 +0200

    Fianl 7

--- current_calc.py
+++ current_calc.py
@@ -1,36 +1,46 @@
 from flask import Flask, request, render_template
 import requests
+import csv
+import os
 
 app = Flask(__name__)
 
-exchange_rates = []
+CSV_FILE = 'exchange_rates.csv'
 
 def fetch_exchange_rates():
-    global exchange_rates
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-    data = response.json()
-    exchange_rates = data[0]['rates']
+    data = response.json()[0]['rates']
+    with open(CSV_FILE, 'w', newline='') as csvfile:
+        fieldnames = ['code', 'bid']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for rate in data:
+            writer.writerow({'code': rate['code'], 'bid': rate['bid']})
 
-fetch_exchange_rates()
+if not os.path.exists(CSV_FILE):
+    fetch_exchange_rates()
+
+exchange_rates = {}
+with open(CSV_FILE, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        exchange_rates[row['code']] = float(row['bid'])
 
 @app.route("/", methods=['GET', 'POST'])
 def curr_calc():
-    global exchange_rates
-    
     if request.method == 'GET':
-        codes = [c["code"] for c in exchange_rates]
+        codes = list(exchange_rates.keys())
         return render_template("calc_form.html", codes=codes) 
     
     elif request.method == 'POST':
         select_curr = request.form.get("currencies")
         exchange_amount = float(request.form.get("element"))
         
-        for rate in exchange_rates:
-            if rate["code"] == select_curr:
-                select_bid = rate["bid"]
-                break
-        
+        select_bid = exchange_rates.get(select_curr)
+        if select_bid is None:
+            return "Kod waluty nieprawidłowy"
+                
         transaction_cost = select_bid * exchange_amount
        
-        codes = [c["code"] for c in exchange_rates]
+        codes = list(exchange_rates.keys())
         return render_template("calc_form.html", codes=codes, transaction_cost=transaction_cost)
