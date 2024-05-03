7ba128f0525451eeac1e0ffd0e77604028029bd4
Michał Zatorski <michal_zatorski@o2.pl>
2024-05-03 21:11:12 +0200

    Fianl 6

--- current_calc.py
+++ current_calc.py
@@ -3,26 +3,34 @@ import requests
 
 app = Flask(__name__)
 
-def get_exchange_rates():
+exchange_rates = []
+
+def fetch_exchange_rates():
+    global exchange_rates
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
     data = response.json()
-    return data[0]['rates']
+    exchange_rates = data[0]['rates']
+
+fetch_exchange_rates()
 
 @app.route("/", methods=['GET', 'POST'])
 def curr_calc():
-    codes = [c["code"] for c in get_exchange_rates()]
+    global exchange_rates
+    
     if request.method == 'GET':
+        codes = [c["code"] for c in exchange_rates]
         return render_template("calc_form.html", codes=codes) 
     
     elif request.method == 'POST':
         select_curr = request.form.get("currencies")
         exchange_amount = float(request.form.get("element"))
         
-        for i in get_exchange_rates():
-            if i["code"] == select_curr:
-                select_bid = i["bid"]
-                
+        for rate in exchange_rates:
+            if rate["code"] == select_curr:
+                select_bid = rate["bid"]
+                break
+        
         transaction_cost = select_bid * exchange_amount
        
-        return render_template("calc_form.html", codes=codes, transaction_cost=transaction_cost) 
-    
+        codes = [c["code"] for c in exchange_rates]
+        return render_template("calc_form.html", codes=codes, transaction_cost=transaction_cost)
