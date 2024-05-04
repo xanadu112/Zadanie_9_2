74e097026d21dbe9a4079be2a1d26395f865c732
Michał Zatorski <michal_zatorski@o2.pl>
2024-05-04 23:02:30 +0200

    Final 8

--- current_calc.py
+++ current_calc.py
@@ -10,29 +10,39 @@ CSV_FILE = 'exchange_rates.csv'
 def fetch_exchange_rates():
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
     data = response.json()[0]['rates']
+    
+    exchange_rates = {}
     with open(CSV_FILE, 'w', newline='') as csvfile:
         fieldnames = ['code', 'bid']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for rate in data:
             writer.writerow({'code': rate['code'], 'bid': rate['bid']})
-
-if not os.path.exists(CSV_FILE):
-    fetch_exchange_rates()
+            exchange_rates[rate['code']] = float(rate['bid'])
+    
+    return exchange_rates
 
 exchange_rates = {}
-with open(CSV_FILE, 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        exchange_rates[row['code']] = float(row['bid'])
+if os.path.exists(CSV_FILE):
+    with open(CSV_FILE, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            exchange_rates[row['code']] = float(row['bid'])
+
+if not exchange_rates:
+    exchange_rates = fetch_exchange_rates()
 
 @app.route("/", methods=['GET', 'POST'])
 def curr_calc():
+    global exchange_rates
+    
     if request.method == 'GET':
         codes = list(exchange_rates.keys())
         return render_template("calc_form.html", codes=codes) 
     
     elif request.method == 'POST':
+        exchange_rates = fetch_exchange_rates()
+        
         select_curr = request.form.get("currencies")
         exchange_amount = float(request.form.get("element"))
         
